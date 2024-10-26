3d1e18ce37f137b03c5e83f118871527ccd4314b
Louis-He <siweihe.eng@gmail.com>
2024-10-25 22:34:00 -0400

    remove token

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,2 @@
-// Keep Token, and ASTNode private and they will be used by parser in the future
-
-mod token;
-
+// Keep ASTNode private and they will be used by parser in the future
 mod ast_node;
