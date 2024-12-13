6b9f998cd32d5171eb88a3c0a1ddd4f7a203ab85
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-12-12 19:26:23 -0500

    Remove naive ast

--- src/parser/ast_node/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod ast_node;
-pub mod ast_node_concat;
-pub mod ast_node_group;
-pub mod ast_node_literal;
-pub mod ast_node_optional;
-pub mod ast_node_plus;
-pub mod ast_node_star;
-pub mod ast_node_union;
