a277fe27160b6ac425352432795411491ddbd955
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-12-15 21:05:13 -0500

    Add commandline option to the example program

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -2,12 +2,30 @@ use log_surgeon::error_handling::Result;
 use log_surgeon::log_parser::LogParser;
 use log_surgeon::parser::SchemaConfig;
 
+use clap::{Arg, Command};
+
 fn main() -> Result<()> {
-    let project_root = env!("CARGO_MANIFEST_DIR");
-    let schema_path = std::path::Path::new(project_root).join("schema.yaml");
-    let log_path = std::path::Path::new(project_root)
-        .join("logs")
-        .join("hive-24h.log");
+    let matches = Command::new("log-surgeon-example")
+        .version("1.0")
+        .arg(
+            Arg::new("schema")
+                .help("Path to the schema file")
+                .required(true)
+                .value_name("SCHEMA_FILE"),
+        )
+        .arg(
+            Arg::new("input")
+                .help("Paths to the input file")
+                .required(true)
+                .value_name("INPUT_FILE"),
+        )
+        .get_matches();
+
+    let schema_path: &String = matches.get_one("schema").expect("no schema found");
+    let input_file: &String = matches.get_one("input").expect("no input file found");
+
+    let schema_path = std::path::Path::new(schema_path.as_str());
+    let log_path = std::path::Path::new(input_file.as_str());
 
     let parsed_schema = SchemaConfig::parse_from_file(schema_path.to_str().unwrap())?;
     let mut log_parser = LogParser::new(parsed_schema.clone())?;
